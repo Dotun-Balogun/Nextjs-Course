bdb238199777821c6c5b2e7953c6ffcaadf61f63
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-01-11 12:54:19 +0100

    intercepted route

--- app/photo-feed/[id]/page.tsx
+++ app/photo-feed/[id]/page.tsx
@@ -3,8 +3,8 @@ import photos from '../photo'
 import Image from 'next/image'
 
 const DetailPhoto = ({params}:{params:{id:string}}) => {
-  const photo = photos.find((p) => p.id === parseInt(params.id))
-  if (!photo) return <div>Photo not found</div>
+  const photo = photos.find((p) => p.id === parseInt(params.id))!
+  // if (!photo) return <div>Photo not found</div>
   return (
     <div className='container mx-auto px-4 py-20 flex flex-col items-center justify-center'>
 <div className='h-96 w-96 mb-6'>
