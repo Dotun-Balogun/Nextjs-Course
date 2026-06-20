29187ffb182ff20fb041d3aa7d8b0737abb69ce3
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-06-20 07:54:20 +0100

    feat:parallel fetching

--- app/parallelFetching/page.tsx
+++ app/parallelFetching/page.tsx
@@ -14,13 +14,12 @@ export type Album = {
 
 export async function getUserProfile(userId: string){
     await new Promise(resolve => setTimeout(resolve, 2000)); 
-   return fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-            .then(res => { return res.json(); });
+   return (await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)).json()
+           
 }
 
 export async function getUserAlbums(userId: string){
     await new Promise(resolve => setTimeout(resolve, 2000));
-return fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)
-            .then(res => { return res.json(); });
+return (await fetch(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`)).json()
 }
 
